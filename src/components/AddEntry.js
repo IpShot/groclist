4770e6d6921e8a3e03c8aa3356136c1efeb72456
IpShot <zhernosek.roman@gmail.com>
2020-09-19 11:38:49 +0500

    add priority

--- src/components/AddEntry.js
+++ src/components/AddEntry.js
@@ -3,17 +3,22 @@ import { AppContext } from '../contexts'
 
 const AddEntry = () => {
   const [input, setInput] = useState('')
+  const [priority, setPriority] = useState(1)
   const [_, actions] = useContext(AppContext)
 
   function handleAddEntry() {
     if (input.trim()) {
-      actions.addEntry(input)
+      actions.addEntry(input, priority)
       setInput('')
     }
   }
+  
   return (
     <div>
       <input value={input} onChange={e => setInput(e.target.value)} />
+      <select onChange={e => setPriority(e.target.value)}>
+        {[1, 2, 3, 4, 5].map(i => <option key={i} value={i}>{i}</option>)}
+      </select>
       <button className="add-entry" onClick={handleAddEntry}>
         Add Entry
       </button>
