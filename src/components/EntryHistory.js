877827a30c5b535019e868a8a1b201314e9489f8
IpShot <zhernosek.roman@gmail.com>
2020-09-19 13:10:12 +0500

    add no history text

--- src/components/EntryHistory.js
+++ src/components/EntryHistory.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const EntryHistory = ({ history }) => (
   <div className="entry-history">
+    {!history.length && 'No status change history'}
     {history.map((d, i) => <div key={i}>{d.toString()}</div>)}
   </div>
 )
