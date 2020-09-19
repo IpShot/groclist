6e2259e130d7040486678e5b27d14ac6f1447893
IpShot <zhernosek.roman@gmail.com>
2020-09-19 10:17:06 +0500

    connect app context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useReducer } from 'react'
+import React, { useMemo, useReducer } from 'react'
 import AddEntry from './components/AddEntry'
 import EntriesList from './components/EntriesList'
 import Filters from './components/Filters'
 import appReducer from './reducers/appReducer'
+import { AppContext } from './contexts'
+import * as actions from './actions'
 import { FILTER } from './constants'
 import './styles.css'
 
@@ -11,14 +13,35 @@ const initialState = {
   filter: FILTER.ALL
 }
 
+function useActions(dispatch) {
+  return useMemo(() => ({
+    addEntry(content, priority) {
+      dispatch(actions.addEntry(content, priority))
+    },
+    toggleEntry(id) {
+      dispatch(actions.toggleEntry(id))
+    },
+    removeEntry(id) {
+      dispatch(actions.removeEntry(id))
+    },
+    setFilter(filter) {
+      dispatch(actions.setFilter(filter))
+    }
+  }), [dispatch])
+}
+
 export default function App() {
   const [state, dispatch] = useReducer(appReducer, initialState)
+  const appActions = useActions(dispatch)
+  const context = useMemo(() => [state, appActions], [state, appActions])
   return (
-    <div className="app">
-      <h1>Groclist</h1>
-      <AddEntry />
-      <EntriesList />
-      <Filters />
-    </div>
+    <AppContext.Provider value={context}>
+      <div className="app">
+        <h1>Groclist</h1>
+        <AddEntry />
+        <EntriesList />
+        <Filters />
+      </div>
+    </AppContext.Provider>
   )
 }
